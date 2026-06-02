209900034264a0a7754a31053376eda903bb356c
irfan-wahyu <irfanwahyu559@gmail.com>
2026-06-02 23:37:25 +0700

    fix deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-   
-   export default defineConfig({
-     site: 'https://irfan-wahyu.github.io',
-     base: '/irfan.site',
-     output: 'static',
-   });
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://irfan-wahyu.github.io",
+  base: "/irfan.site/",
+  output: "static",
+});
