f1b07c553b5389593c96648fc5376582129c977c
irfan-wahyu <irfanwahyu559@gmail.com>
2026-06-03 16:43:00 +0700

    benerin fix bener plis

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import keystatic from "@keystatic/astro";
 
 export default defineConfig({
   output: "static",
-  base: "/ruang-wahyu/",
-  integrations: [react(), import.meta.env.DEV && keystatic()].filter(Boolean),
+  base: "/irfan.site/", // Pastikan nama ini sama persis dengan nama repositori GitHub kamu
+  integrations: [react(), keystatic()],
 });
