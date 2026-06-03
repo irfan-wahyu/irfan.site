5228beb9f80c4a84b2ecb868a598580d609f1cb1
irfan-wahyu <irfanwahyu559@gmail.com>
2026-06-03 16:59:38 +0700

    alhamdulillah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import keystatic from "@keystatic/astro";
-
-// Kita deteksi apakah kita sedang nge-build atau lagi ngoding biasa
-const isBuild = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   output: "static",
   base: "/irfan.site/",
-  integrations: [
-    react(),
-    // Keystatic HANYA dijalankan kalau BUKAN lagi proses build
-    !isBuild && keystatic(),
-  ].filter(Boolean), // Membersihkan array agar tidak ada nilai 'false'
+  integrations: [react()],
 });
