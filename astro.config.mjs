e6e816e6ce274bd4a5da806a7e8be8cd9a7d0de9
irfan-wahyu <irfanwahyu559@gmail.com>
2026-06-03 16:06:11 +0700

    Fix: Explicitly set static output and undefined adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== "production";
 export default defineConfig({
   site: "https://irfan-wahyu.github.io",
   base: isDev ? "/" : "/irfan.site/",
-  integrations: [react(), keystatic()],
   output: "static",
+  adapter: undefined,
+  integrations: [react(), keystatic()],
 });
