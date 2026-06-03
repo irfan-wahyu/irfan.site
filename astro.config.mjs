c6c5e4fe0f577ac01409ff520ed232ac50ee9d8e
irfan-wahyu <irfanwahyu559@gmail.com>
2026-06-03 16:34:16 +0700

    cms mode dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
 
 export default defineConfig({
   output: "static",
-  base: "/irfan.site/",
-  integrations: [react()],
+  base: "/ruang-wahyu/",
+  integrations: [react(), import.meta.env.DEV && keystatic()].filter(Boolean),
 });
