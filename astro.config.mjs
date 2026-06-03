b35b9ff7eab8d672316d9e16cc1e488114f4db13
irfan-wahyu <irfanwahyu559@gmail.com>
2026-06-03 16:24:50 +0700

    Fix: Setup build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from "astro/config";
-import keystatic from "@keystatic/astro";
 import react from "@astrojs/react";
 
-const isDev = process.env.NODE_ENV !== "production";
-
 export default defineConfig({
-  site: "https://irfan-wahyu.github.io",
-  base: isDev ? "/" : "/irfan.site/",
   output: "static",
-  adapter: undefined,
-  integrations: [react(), keystatic()],
+  integrations: [react()],
 });
